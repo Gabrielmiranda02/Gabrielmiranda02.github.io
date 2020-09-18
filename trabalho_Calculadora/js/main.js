44edb668ad34de7750b423ea321f976ec49cb470
Gabrielmiranda02 <69901823+Gabrielmiranda02@users.noreply.github.com>
2020-09-17 22:27:10 -0300

    adicionar trabalho_calculadora

---
+++ trabalho_Calculadora/js/main.js
@@ -0,0 +1,13 @@
+var valor;
+var resultado;
+ function botao(num){
+    valor = document.calc.visor.value += num;
+
+}
+function reset(){
+    document.calc.visor.value = '';
+}
+function calcula(){
+    resultado = eval(valor);
+    document.calc.visor.value = resultado; 
+}
